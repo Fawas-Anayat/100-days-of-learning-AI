bffb78c4de328f024891ee2b4c28960079f9ffcd
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-09-02 17:13:20 +0500

    adding code

---
+++ Day_5_of_Learning_AI/code.py
@@ -0,0 +1,2 @@
+# the complete code for the topics of the day 5/100 is present in my dedicated repository of the statistics.You can access today'code at :
+https://github.com/Fawas-Anayat/statistics_for_machine_learning/blob/main/inferential_stats.ipynb
