11c2b80e210ec8afeb51f12a77e62d9094e8cd17
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-09-04 22:09:06 +0500

    last commit of day 7

---
+++ Day_7_of_Learning_AI/code.py
@@ -0,0 +1,2 @@
+# The complete code of my practice today is available on my dedicated repository of the data preprocessing,which you can access at (https://github.com/Fawas-Anayat/data_preprocessing/blob/main/data_pipline2.ipynb)
+ # and at (https://github.com/Fawas-Anayat/data_preprocessing/blob/main/data_transformer2.ipynb)
