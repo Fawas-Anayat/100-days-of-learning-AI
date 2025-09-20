5677d0a3928d1e8573f5ba9c25fe9e88cb96da57
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-09-19 22:56:21 -0700

    pandas learning

---
+++ Day_23_of_Learning_AI/code.py
@@ -0,0 +1,2 @@
+## you can access the code at my specialized repository of the data preprocessing 
+## which is at the link ....https://github.com/Fawas-Anayat/data_preprocessing/blob/main/pandas_lerarning.ipynb
