8653ab5f977db77d646d974c3670e4b99f86a9ef
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-09-20 15:44:59 -0700

    day 24 final commit

---
+++ Day_24_of_Learning_AI/code.py
@@ -0,0 +1,3 @@
+## the complete code is present at ---https://github.com/Fawas-Anayat/data_preprocessing/blob/main/pandas_learning_part2.ipynb
+
+## and at this link---https://github.com/Fawas-Anayat/data_preprocessing/blob/main/pandas_lerarning.ipynb
