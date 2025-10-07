a462a41b501b087b0f54d1b626c161923ff9ae97
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-10-07 22:50:01 +0500

    day 51 LR code

---
+++ Day_51_of_Learning_AI/code.py
@@ -0,0 +1,4 @@
+## To practice the linear regression and the evalustion metrics and the model fine tunning i have created a mini project that is 
+## present in my github repo of the machine learning algorithms from where you can access it. the link is as follow
+
+## https://github.com/Fawas-Anayat/Machine_Learning_Algorithms/tree/main/Linear_regression2
